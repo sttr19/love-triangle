10d5c5e6d12386f8054c85ace938577f32c4d43c
sttr19 <stastroshko@gmail.com>
2019-03-24 10:29:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,33 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = function getLoveTrianglesCount(preferences = []) 
+{
+           var length = preferences.length;
+            var count = 0;
+         for(var i=0;i<length;i++)
+         {
+        if(preferences[i]!=i+1&&preferences[i]!=0)
+        {
+        for(var j=0;j<length;j++)
+        {
+       if(preferences[i]===j+1)
+        {     
+       var item=preferences[j];
+       for(var k=0;k<length;k++)
+       {
+        if(item===k+1&&preferences[k]===i+1) 
+        count++;
+           else continue;
+           break;
+        }break;
+      }
+       else continue;
+       
+   }
+  }
+ }
+  var g=count/3;
+  return Math.trunc(g);
+
+}
